322df53875cf7558c9cfa8d946da8e5826adb375
orlandev <onovas05@gmail.com>
2023-03-17 15:21:43 -0400

    clean

--- app/src/main/java/com/orlandev/compose_template/SharedViewModel.kt
+++ app/src/main/java/com/orlandev/compose_template/SharedViewModel.kt
@@ -3,7 +3,6 @@ package com.orlandev.compose_template
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
